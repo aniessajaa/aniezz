75298a31050ddb410fa7194db901be730621ac36
aniessajaa <annisaaaaawjyy26@gmail.com>
2025-08-06 13:05:11 +0700

    Add files via upload

---
+++ bab.3.java/src/bab/pkg3/java/OptionPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bab.pkg3.java;
+
+/**
+ *
+ * @author ardiw
+ */
+class OptionPane {
+
+    static void showMessageDialog(Object object, String kamu_memilih_Yes) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
