a7ac6f53c8dec1ee95bc92831ff68b9d2c1cffa9
aniessajaa <annisaaaaawjyy26@gmail.com>
2025-08-20 08:41:56 +0700

    Add files via upload

---
+++ BAB9java.java/src/bab9java/java/hewan.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bab9java.java;
+
+/**
+ *
+ * @author ardiw
+ */
+public class hewan {
+    public static void main(String[] args) {
+     hewan anjing = new hewan();
+}
+}
